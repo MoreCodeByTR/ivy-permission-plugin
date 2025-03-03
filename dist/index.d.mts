dcbf1df121072e44a81013b6e3c5644a0e22149b
tianrui0526 <tianrui0526@wepie.com>
2025-03-03 11:25:13 +0800

    chore: debug

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,3 +1,23 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+import React from 'react';
+
+interface PermissionProviderProps {
+    roles: string[];
+    permissions: string[];
+    children: React.ReactNode;
+}
+declare const PermissionProvider: ({ roles, permissions, children }: PermissionProviderProps) => react_jsx_runtime.JSX.Element;
+
+declare const usePermission: (requiredRoles?: string[], requiredPermissions?: string[]) => boolean;
+
+type PermissionProps = {
+    roles?: string[];
+    permissions?: string[];
+    children: React.ReactNode;
+    fallback?: React.ReactNode;
+};
+declare const Permission: ({ roles, permissions, children, fallback }: PermissionProps) => react_jsx_runtime.JSX.Element;
+
 declare const IVY_VERSION = "1.0.0";
 
-export { IVY_VERSION };
+export { IVY_VERSION, Permission, PermissionProvider, usePermission };
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,5 +1,39 @@
+// src/context.tsx
+import { createContext, useContext } from "react";
+import { jsx } from "react/jsx-runtime";
+var PermissionContext = createContext(null);
+var PermissionProvider = ({ roles, permissions, children }) => {
+  return /* @__PURE__ */ jsx(PermissionContext.Provider, { value: { roles, permissions }, children });
+};
+var usePermissionContext = () => {
+  const context = useContext(PermissionContext);
+  if (!context) {
+    throw new Error("usePermissionContext must be used within a PermissionProvider");
+  }
+  return context;
+};
+
+// src/usePermission.ts
+var usePermission = (requiredRoles = [], requiredPermissions = []) => {
+  console.log("usePermission===>");
+  const { roles, permissions } = usePermissionContext();
+  const hasRole = requiredRoles.length === 0 || requiredRoles.some((role) => roles.includes(role));
+  const hasPermission = requiredPermissions.length === 0 || requiredPermissions.some((permission) => permissions.includes(permission));
+  return hasRole && hasPermission;
+};
+
+// src/Permission.tsx
+import { Fragment, jsx as jsx2 } from "react/jsx-runtime";
+var Permission = ({ roles = [], permissions = [], children, fallback = null }) => {
+  const hasAccess = usePermission(roles, permissions);
+  return /* @__PURE__ */ jsx2(Fragment, { children: hasAccess ? children : fallback });
+};
+
 // src/constant.ts
 var IVY_VERSION = "1.0.0";
 export {
-  IVY_VERSION
+  IVY_VERSION,
+  Permission,
+  PermissionProvider,
+  usePermission
 };
